364e4cde20d319c2536622874147d70ae64b472e
nithila <nithila.annadurai@uconn.edu>
2023-09-18 23:33:54 -0400

    edits files

--- heatmap_pos_obs.py
+++ heatmap_pos_obs.py
@@ -2,87 +2,90 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pylab as plt
+import interpolation as ip
 
-#checks if input is null - returns True if not null
-def notNull(x):
-    return (x[1] == x[1])
+# #checks if input is null - returns True if not null
+# def notNull(x):
+#     return (x[1] != None)
 
-#interpolation function - takes in 2 tuples of (index, value) and returns a list of tuples with interpolates values
-def interpolate_mid(pair1, pair2):
-    first_idx = pair1[0]
-    first_val = pair1[1]
-    sec_idx = pair2[0]
-    sec_val = pair2[1]
+# #interpolation function - takes in 2 tuples of (index, value) and returns a list of tuples with interpolates values
+# def interpolate_mid(pair1, pair2):
+#     first_idx = pair1[0]
+#     first_val = pair1[1]
+#     sec_idx = pair2[0]
+#     sec_val = pair2[1]
 
-    dist = sec_val - first_val
-    idx_dist = sec_idx - first_idx
+#     dist = sec_val - first_val
+#     idx_dist = sec_idx - first_idx
 
-    step = dist / idx_dist
+#     step = dist / idx_dist
 
-    filled = []
+#     filled = []
 
-    for i in range(idx_dist):
-        filled.append((first_idx+i, first_val+(step*i)))
+#     for i in range(idx_dist):
+#         filled.append((first_idx+i, first_val+(step*i)))
 
-    return filled
-
-
-def heatmap(csv):
-    data_frame = pd.read_csv('ibns csv/' + csv) #might have to change based on your directory name
+#     return filled
 
 
-    head_coords = data_frame.iloc[2:,1:4].copy() #obs nose x, y, likelihood coords
-    print(head_coords.shape) # 10 frames per second 
-
-    #gets list of column names
-    col_names = list(head_coords.columns)
-
-    #filter out low likelihoods for the head coordinates
-    for i in range(head_coords.shape[1]):
-        head_coords[col_names[i]] =  pd.to_numeric(head_coords[col_names[i]])
+# def heatmap(csv):
+#     data_frame = pd.read_csv('ibns csv/' + csv) #might have to change based on your directory name
 
 
-    #filter out low likelihoods for the head coordinates
-    for row in range(head_coords.shape[0]):
-        if head_coords.iloc[row,2] < 0.95:
-            head_coords.iloc[row,0] = None
-            head_coords.iloc[row,1] = None
+#     head_coords = data_frame.iloc[2:,1:4].copy() #obs nose x, y, likelihood coords
+#     print(head_coords.shape) # 10 frames per second 
 
-    #enumerates the x and y coordinates (makes a list of tuples -> (index, value))
-    list_head_coords_x = enumerate(list(head_coords.iloc[:, 0]))
-    list_head_coords_y = enumerate(list(head_coords.iloc[:, 1]))
+#     #gets list of column names
+#     col_names = list(head_coords.columns)
 
-    #filters out nulls
-    filtered_x = list(filter(notNull, list_head_coords_x))
-    filtered_y = list(filter(notNull, list_head_coords_y))
+#     #filter out low likelihoods for the head coordinates
+#     for i in range(head_coords.shape[1]):
+#         head_coords[col_names[i]] =  pd.to_numeric(head_coords[col_names[i]])
+
+
+#     #filter out low likelihoods for the head coordinates
+#     for row in range(head_coords.shape[0]):
+#         if head_coords.iloc[row,2] < 0.95:
+#             head_coords.iloc[row,0] = None
+#             head_coords.iloc[row,1] = None
+
+#     #enumerates the x and y coordinates (makes a list of tuples -> (index, value))
+#     list_head_coords_x = enumerate(list(head_coords.iloc[:, 0]))
+#     list_head_coords_y = enumerate(list(head_coords.iloc[:, 1]))
+
+#     #filters out nulls
+#     filtered_x = list(filter(notNull, list_head_coords_x))
+#     filtered_y = list(filter(notNull, list_head_coords_y))
    
 
-   # print(list(filter(notNull, list_head_coords)))
+#    # print(list(filter(notNull, list_head_coords)))
 
-    #interpolates remaining values
-    interpolated_x = interpolate_mid(filtered_x[0], filtered_x[1])
-    interpolated_y = interpolate_mid(filtered_y[0], filtered_y[1])
+#     #interpolates remaining values
+#     interpolated_x = interpolate_mid(filtered_x[0], filtered_x[1])
+#     interpolated_y = interpolate_mid(filtered_y[0], filtered_y[1])
 
-    all_x = []
-    all_y = []
-    #interpolates remaining values
-    for i in range(1, len(filtered_x)):
-        interpolated_x = interpolate_mid(filtered_x[i-1], filtered_x[i])
-        interpolated_y = interpolate_mid(filtered_y[i-1], filtered_y[i])
-        all_x.append(interpolated_x)
-        all_y.append(interpolated_y)
+#     all_x = []
+#     all_y = []
+#     #interpolates remaining values
+#     for i in range(1, len(filtered_x)):
+#         interpolated_x = interpolate_mid(filtered_x[i-1], filtered_x[i])
+#         interpolated_y = interpolate_mid(filtered_y[i-1], filtered_y[i])
+#         all_x.append(interpolated_x)
+#         all_y.append(interpolated_y)
 
-    #iterates to get just the values for the x and y coordinates (without the indices)
-    x_col = []
-    for i in all_x:
-        for j in i:
-            x_col.append(j)
+#     #iterates to get just the values for the x and y coordinates (without the indices)
+#     x_col = []
+#     for i in all_x:
+#         for j in i:
+#             x_col.append(j)
 
-    y_col = []
-    for i in all_y:
-        for j in i:
-            y_col.append(j)
-
+#     y_col = []
+#     for i in all_y:
+#         for j in i:
+#             y_col.append(j)
+def heatmap(csv):
+    
+    x_col, y_col = ip.interpolate(csv, 1, 4)
     #the frames for the main session - not including the first and last 5 mins (these frame numbers might change)
     main_sesh_x = x_col[3000:6200]
     main_sesh_y = y_col[3000:6200]
@@ -132,5 +135,4 @@ def heatmap(csv):
     plt.title('Full Trial (Learner Correct) - Teacher') #change this based on what you are generating heatmap for
     plt.show() 
 
-heatmap("05252023 98 + 117DeepCut_resnet50_IBNS Social AERIALJun10shuffle1_255000.csv") #change this based on what CSV file you are generating heatmap for
-
+heatmap("MATLAB/ibns csv/05252023 98 + 117DeepCut_resnet50_IBNS Social AERIALJun10shuffle1_255000.csv") #change this based on what CSV file you are generating heatmap for
